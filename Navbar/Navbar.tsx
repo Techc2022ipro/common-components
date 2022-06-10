a37012aacf2198f9928295ad1abaf6bc578571df
sabi1125 <sabir_barahi4@hotmail.com>
2022-06-11 02:52:48 +0900

    added home

--- Navbar/Navbar.tsx
+++ Navbar/Navbar.tsx
@@ -1,6 +1,12 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
 const Navbar = () => {
     return (
+        <Router>
         <a href="http://localhost:8080">Login</a>
+        <a href="http://localhost:7070">Home</a>
+        </Router>
     )
 }
 
