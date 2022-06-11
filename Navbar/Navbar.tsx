8e93a1a0f2804c0dbb799026d2aaab35ca86082e
sabi1125 <sabir_barahi4@hotmail.com>
2022-06-11 16:40:37 +0900

    fixed navbar

--- Navbar/Navbar.tsx
+++ Navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const Navbar = () => {
     return (
         <Router>
-        <a href="http://localhost:8080">Login</a>
         <a href="http://localhost:7070">Home</a>
+        <a href="http://localhost:8080">Login</a>
         </Router>
     )
 }
