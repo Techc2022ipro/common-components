441d20e9a6207763065be1343fe9b5515288185e
sabi1125 <sabir_barahi4@hotmail.com>
2022-07-20 21:02:08 +0900

    navbar style

--- Navbar/Navbar.tsx
+++ Navbar/Navbar.tsx
@@ -4,9 +4,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const Navbar = () => {
     return (
         <Router>
-        <a href="http://localhost:7070">Home</a>
-        <a href="http://localhost:2020">Product</a>
-        <a href="http://localhost:8080">Login</a>
+        <div className='nav'>
+        <div className='logo'>Lookbook</div>
+        <div className='links'>
+          <a href="http://localhost:7070" className='link'>Home</a>
+          <a href="http://localhost:2020" className='link'>Product</a>
+          <a href="http://localhost:8080" className='link'>Login</a>
+        </div>
+        </div>
         </Router>
     )
 }
